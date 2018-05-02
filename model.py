196650c1329c2d1413dd7ee85ac4a6cfccdfb821
Tatari-p <tatari.lugh@gmail.com>
2018-05-03 02:20:53 +0900

    added

--- model.py
+++ model.py
@@ -43,112 +43,46 @@ def downsample(x, filters, training):
     return h
 
 
-def create_resnet_18(name, x, training, num_outputs, reuse=False):
-    num_repeats = [2, 2, 2, 2]
+def create_resnet(name, x, training, num_blocks, num_outputs, reuse=False, bottleneck=False):
+    create_block = create_bottleneck_block if bottleneck else create_resnet_block
+    unit = 256 if bottleneck else 64
 
     with tf.variable_scope(name, reuse=reuse):
         b = layers.conv2d(x, 64, kernel_size=7, strides=2, activation=tf.nn.elu, padding="SAME")
         b = layers.max_pooling2d(b, pool_size=3, strides=2, padding="SAME")
 
-        for i, repeat in enumerate(num_repeats):
-            b = create_resnet_block(b, 64*2**i, training, skip_connection=True)
+        for i, num_repeats in enumerate(num_blocks):
+            b = create_block(b, unit*2**i, training, skip_connection=True)
 
-            for _ in range(repeat-1):
-                b = create_resnet_block(b, 64*2**i, training)
+            for _ in range(num_repeats-1):
+                b = create_block(b, unit*2**i, training)
 
-            b = downsample(b, 64*2**(i+1), training)
+            b = downsample(b, unit*2**(i+1), training)
 
+        # use global average pooling
         b = layers.conv2d(b, num_outputs, kernel_size=1, activation=tf.nn.elu, padding="SAME")
         b = tf.reduce_mean(b, [1, 2])
         fts = tf.squeeze(b)
 
     return fts
+
+
+def create_resnet_18(name, x, training, num_outputs, reuse=False):
+    return create_resnet(name, x, training, [2, 2, 2, 2], num_outputs, reuse)
 
 
 def create_resnet_34(name, x, training, num_outputs, reuse=False):
-    num_repeats = [3, 4, 6, 3]
-
-    with tf.variable_scope(name, reuse=reuse):
-        b = layers.conv2d(x, 64, kernel_size=7, strides=2, activation=tf.nn.elu, padding="SAME")
-        b = layers.max_pooling2d(b, pool_size=3, strides=2, padding="SAME")
-
-        for i, repeat in enumerate(num_repeats):
-            b = create_resnet_block(b, 64*2**i, training, skip_connection=True)
-
-            for _ in range(repeat-1):
-                b = create_resnet_block(b, 64*2**i, training)
-
-            b = downsample(b, 64*2**(i+1), training)
-
-        b = layers.conv2d(b, num_outputs, kernel_size=1, activation=tf.nn.elu, padding="SAME")
-        b = tf.reduce_mean(b, [1, 2])
-        fts = tf.squeeze(b)
-
-    return fts
+    return create_resnet(name, x, training, [3, 4, 6, 3], num_outputs, reuse)
 
 
 def create_resnet_50(name, x, training, num_outputs, reuse=False):
-    num_repeats = [3, 4, 6, 3]
-
-    with tf.variable_scope(name, reuse=reuse):
-        b = layers.conv2d(x, 64, kernel_size=7, strides=2, activation=tf.nn.elu, padding="SAME")
-        b = layers.max_pooling2d(b, pool_size=3, strides=2, padding="SAME")
-
-        for i, repeat in enumerate(num_repeats):
-            b = create_bottleneck_block(b, 256*2**i, training, skip_connection=True)
-
-            for _ in range(repeat-1):
-                b = create_bottleneck_block(b, 256*2**i, training)
-
-            b = downsample(b, 256*2**(i+1), training)
-
-        b = layers.conv2d(b, num_outputs, kernel_size=1, activation=tf.nn.elu, padding="SAME")
-        b = tf.reduce_mean(b, [1, 2])
-        fts = tf.squeeze(b)
-
-    return fts
+    return create_resnet(name, x, training, [3, 4, 6, 3], num_outputs, reuse, bottleneck=True)
 
 
 def create_resnet_101(name, x, training, num_outputs, reuse=False):
-    num_repeats = [3, 4, 23, 3]
-
-    with tf.variable_scope(name, reuse=reuse):
-        b = layers.conv2d(x, 64, kernel_size=7, strides=2, activation=tf.nn.elu, padding="SAME")
-        b = layers.max_pooling2d(b, pool_size=3, strides=2, padding="SAME")
-
-        for i, repeat in enumerate(num_repeats):
-            b = create_bottleneck_block(b, 256*2**i, training, skip_connection=True)
-
-            for _ in range(repeat-1):
-                b = create_bottleneck_block(b, 256*2**i, training)
-
-            b = downsample(b, 256*2**(i+1), training)
-
-        b = layers.conv2d(b, num_outputs, kernel_size=1, activation=tf.nn.elu, padding="SAME")
-        b = tf.reduce_mean(b, [1, 2])
-        fts = tf.squeeze(b)
-
-    return fts
+    return create_resnet(name, x, training, [3, 4, 23, 3], num_outputs, reuse, bottleneck=True)
 
 
 def create_resnet_152(name, x, training, num_outputs, reuse=False):
-    num_repeats = [3, 8, 36, 3]
-
-    with tf.variable_scope(name, reuse=reuse):
-        b = layers.conv2d(x, 64, kernel_size=7, strides=2, activation=tf.nn.elu, padding="SAME")
-        b = layers.max_pooling2d(b, pool_size=3, strides=2, padding="SAME")
-
-        for i, repeat in enumerate(num_repeats):
-            b = create_bottleneck_block(b, 256*2**i, training, skip_connection=True)
-
-            for _ in range(repeat-1):
-                b = create_bottleneck_block(b, 256*2**i, training)
-
-            b = downsample(b, 256*2**(i+1), training)
-
-        b = layers.conv2d(b, num_outputs, kernel_size=1, activation=tf.nn.elu, padding="SAME")
-        b = tf.reduce_mean(b, [1, 2])
-        fts = tf.squeeze(b)
-
-    return fts
+    return create_resnet(name, x, training, [3, 8, 36, 3], num_outputs, reuse, bottleneck=True)
 
